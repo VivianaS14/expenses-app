51c5cdb6d734e536e9697e2a954528797fe68a6d
VivianaS14 <vasorianp@gmail.com>
2023-12-28 17:28:34 -0500

    Set up project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+
+import { Provider, Button } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Provider>
+        <Text>Open up App.tsx to start working on your app!</Text>
+
+        <Button mode="contained" onPress={() => console.log("Pressed")}>
+          Press Me!
+        </Button>
+        <StatusBar style="auto" />
+      </Provider>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
   },
 });
