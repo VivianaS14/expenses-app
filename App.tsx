1fcaf1a2436dadf2e89f2dfc879bb3455a020786
VivianaS14 <vasorianp@gmail.com>
2024-01-03 13:35:20 -0500

    Add modal to create new expense

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import {
   StyleSheet,
   SafeAreaView,
@@ -21,6 +21,11 @@ import AllExpenses from "./screens/AllExpenses";
 import Expense from "./screens/Expense";
 
 import { RootParamList } from "./types/Navigation";
+import { Provider, useDispatch } from "react-redux";
+import { store } from "./store";
+import ModalNewExpense from "./components/ModalNewExpense";
+import { Expense as ExpenseType } from "./types/Expense";
+import { newExpense as addNewExpense } from "./features/expenses/expensesSlice";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -29,63 +34,79 @@ const BottomTab = createBottomTabNavigator<RootParamList>();
 const Stack = createNativeStackNavigator<RootParamList>();
 
 function BottomNavigator() {
+  const [isOpenNewExpense, setIsOpenNewExpense] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const newExpenseHandler = (expense: ExpenseType) => {
+    dispatch(addNewExpense(expense));
+  };
+
   return (
-    <BottomTab.Navigator
-      initialRouteName="Recent"
-      sceneContainerStyle={{ backgroundColor: "#FFF7D6" }}
-      screenOptions={{
-        headerStyle: { backgroundColor: "#29556B" },
-        headerTitleStyle: {
-          fontFamily: "Poppins-SemiBold",
-          fontSize: 27,
-          color: "#FFF7D6",
-        },
-        tabBarLabelStyle: {
-          fontFamily: "Poppins-SemiBold",
-          fontSize: 12,
-          bottom: 12,
-        },
-        tabBarActiveTintColor: "#FFF7D6",
-        tabBarInactiveTintColor: "#00BAA3",
-        tabBarStyle: {
-          backgroundColor: "#29556B",
-          height: 70,
-        },
-        headerRight: () => (
-          <Pressable
-            style={({ pressed }) => [
-              styles.menuButton,
-              pressed ? { opacity: 0.5 } : null,
-            ]}
-          >
-            <Ionicons name="add-circle-outline" size={27} color="#FFF7D6" />
-          </Pressable>
-        ),
-      }}
-    >
-      <BottomTab.Screen
-        name="Recent"
-        component={RecentExpenses}
-        options={{
-          tabBarLabel: "Recent",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="hourglass-outline" size={24} color={color} />
+    <>
+      <BottomTab.Navigator
+        initialRouteName="Recent"
+        sceneContainerStyle={{ backgroundColor: "#FFF7D6" }}
+        screenOptions={{
+          headerStyle: { backgroundColor: "#29556B" },
+          headerTitleStyle: {
+            fontFamily: "Poppins-SemiBold",
+            fontSize: 27,
+            color: "#FFF7D6",
+          },
+          tabBarLabelStyle: {
+            fontFamily: "Poppins-SemiBold",
+            fontSize: 12,
+            bottom: 12,
+          },
+          tabBarActiveTintColor: "#FFF7D6",
+          tabBarInactiveTintColor: "#00BAA3",
+          tabBarStyle: {
+            backgroundColor: "#29556B",
+            height: 70,
+          },
+          headerRight: () => (
+            <Pressable
+              style={({ pressed }) => [
+                styles.menuButton,
+                pressed ? { opacity: 0.5 } : null,
+              ]}
+              onPress={() => setIsOpenNewExpense(true)}
+            >
+              <Ionicons name="add-circle-outline" size={27} color="#FFF7D6" />
+            </Pressable>
           ),
-          title: "Recent Expenses",
         }}
+      >
+        <BottomTab.Screen
+          name="Recent"
+          component={RecentExpenses}
+          options={{
+            tabBarLabel: "Recent",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="hourglass-outline" size={24} color={color} />
+            ),
+            title: "Recent Expenses",
+          }}
+        />
+        <BottomTab.Screen
+          name="All"
+          component={AllExpenses}
+          options={{
+            tabBarLabel: "All Expenses",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="calendar" size={24} color={color} />
+            ),
+            title: "All Expenses",
+          }}
+        />
+      </BottomTab.Navigator>
+      <ModalNewExpense
+        isVisible={isOpenNewExpense}
+        onClose={() => setIsOpenNewExpense(false)}
+        onSubmitForm={newExpenseHandler}
       />
-      <BottomTab.Screen
-        name="All"
-        component={AllExpenses}
-        options={{
-          tabBarLabel: "All Expenses",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="calendar" size={24} color={color} />
-          ),
-          title: "All Expenses",
-        }}
-      />
-    </BottomTab.Navigator>
+    </>
   );
 }
 
@@ -112,32 +133,35 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
       <PaperProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
-              headerStyle: { backgroundColor: "#29556B" },
-              headerTitleStyle: {
-                fontFamily: "Poppins-SemiBold",
-                fontSize: 27,
-                color: "#FFF7D6",
-              },
-              headerTitleAlign: "center",
-              headerTintColor: "#FFF7D6",
-              contentStyle: { backgroundColor: "#FFF7D6" },
-            }}
-          >
-            <Stack.Screen name="Buttons" component={BottomNavigator} />
-            <Stack.Screen name="Recent" component={RecentExpenses} />
-            <Stack.Screen
-              name="Expense"
-              component={Expense}
-              options={{
-                headerShown: true,
+        <Provider store={store}>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+                headerStyle: { backgroundColor: "#29556B" },
+                headerTitleStyle: {
+                  fontFamily: "Poppins-SemiBold",
+                  fontSize: 27,
+                  color: "#FFF7D6",
+                },
+                headerTitleAlign: "center",
+                headerTintColor: "#FFF7D6",
+                contentStyle: { backgroundColor: "#FFF7D6" },
               }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
+            >
+              <Stack.Screen name="Buttons" component={BottomNavigator} />
+              <Stack.Screen name="Recent" component={RecentExpenses} />
+              <Stack.Screen
+                name="Expense"
+                component={Expense}
+                options={{
+                  headerShown: true,
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Provider>
+
         <StatusBar style="light" />
       </PaperProvider>
     </SafeAreaView>
