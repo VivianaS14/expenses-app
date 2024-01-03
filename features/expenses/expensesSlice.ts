dd55821c5fad852ff6b968cde55eb9db3d262b01
VivianaS14 <vasorianp@gmail.com>
2024-01-03 15:11:24 -0500

    Update and delete expense

--- features/expenses/expensesSlice.ts
+++ features/expenses/expensesSlice.ts
@@ -6,7 +6,14 @@ export interface State {
 }
 
 const initialState: State = {
-  expenses: [],
+  expenses: [
+    {
+      id: "EX1EOVJJ",
+      subject: "Sun Cream",
+      date: "2024-01-02 12:17",
+      value: 75,
+    },
+  ],
 };
 
 export const expenseSlice = createSlice({
@@ -16,9 +23,26 @@ export const expenseSlice = createSlice({
     newExpense: (state, action: PayloadAction<Expense>) => {
       state.expenses.push(action.payload);
     },
+    updateExpense: (state, action: PayloadAction<Expense>) => {
+      const index = state.expenses.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.expenses[index] = action.payload;
+      }
+    },
+    deleteExpense: (state, action: PayloadAction<string>) => {
+      const index = state.expenses.findIndex(
+        (expense) => expense.id === action.payload
+      );
+      if (index !== -1) {
+        state.expenses.splice(index, 1);
+      }
+    },
   },
 });
 
-export const { newExpense } = expenseSlice.actions;
+export const { newExpense, updateExpense, deleteExpense } =
+  expenseSlice.actions;
 
 export default expenseSlice.reducer;
--- screens/Expense.tsx
+++ screens/Expense.tsx
@@ -1,15 +1,136 @@
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import DateTimePicker, { DateType } from "react-native-ui-datepicker";
+
+import { RootState } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateExpense,
+  deleteExpense,
+} from "../features/expenses/expensesSlice";
+
 import { RootParamList } from "../types/Navigation";
 
 type Props = NativeStackScreenProps<RootParamList, "Expense">;
 
-export default function Expense({ route }: Props) {
+export default function Expense({ route, navigation }: Props) {
+  const { expenses } = useSelector((state: RootState) => state.expenses);
+  const dispatch = useDispatch();
+
   const id = route.params.id;
+  const expense = expenses.find((expense) => expense.id === id)!;
+
+  const [subject, setSubject] = useState(expense?.subject);
+  const [value, setValue] = useState(expense?.value.toString());
+  const [date, setDate] = useState<DateType>(expense?.date);
+
+  const onUpdateExpense = () => {
+    dispatch(
+      updateExpense({
+        id: expense?.id,
+        subject,
+        value: Number(value),
+        date,
+      })
+    );
+
+    navigation.goBack();
+  };
+
+  const onDeleteExpense = () => {
+    dispatch(deleteExpense(id));
+
+    navigation.goBack();
+  };
 
   return (
-    <View>
-      <Text>Expense # {id}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Subject:</Text>
+      <TextInput
+        value={subject}
+        onChangeText={(text) => setSubject(text)}
+        placeholder="Example: A Book"
+        mode="outlined"
+        selectionColor="#00BAA3"
+        activeOutlineColor="#00BAA3"
+        outlineColor="#29556B"
+      />
+
+      <Text style={styles.title}>Value:</Text>
+      <TextInput
+        value={value}
+        onChangeText={(value) => setValue(value)}
+        keyboardType="number-pad"
+        mode="outlined"
+        selectionColor="#00BAA3"
+        activeOutlineColor="#00BAA3"
+        outlineColor="#29556B"
+        left={<TextInput.Icon icon="cash" />}
+      />
+
+      <Text style={styles.title}>Date:</Text>
+      <View style={styles.dateContainer}>
+        <View style={styles.datePicker}>
+          <DateTimePicker
+            mode="date"
+            value={date}
+            onValueChange={(date) => setDate(date)}
+            maximumDate={new Date()}
+            displayFullDays={true}
+            headerButtonColor="#00BAA3"
+            headerContainerStyle={{ paddingBottom: 5 }}
+            selectedItemColor="#00BAA3"
+            selectedTextStyle={{
+              fontFamily: "Poppins-SemiBold",
+            }}
+            todayContainerStyle={{
+              borderWidth: 1,
+            }}
+            calendarTextStyle={{ fontFamily: "Poppins-Medium" }}
+          />
+        </View>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button mode="contained" color="#29556B" onPress={onUpdateExpense}>
+          Update
+        </Button>
+        <Button color="#86363B" onPress={onDeleteExpense}>
+          Delete
+        </Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    gap: 10,
+  },
+
+  title: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 18,
+  },
+
+  dateContainer: {
+    alignItems: "center",
+  },
+
+  datePicker: {
+    width: 330,
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 8,
+    borderColor: "#29556B",
+    borderWidth: 1,
+  },
+
+  buttonsContainer: {
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+});
