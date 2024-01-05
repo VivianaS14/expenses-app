da984278782f74511a736f08d47fca1ca8ff6e2b
VivianaS14 <vasorianp@gmail.com>
2024-01-05 10:03:42 -0500

    Log in and Sign up functions

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
+import { ActivityIndicator } from "react-native-paper";
+
 import AuthContent from "../components/Auth/AuthContent";
 
+import { Credential } from "../types/Auth";
+
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store";
+import { authenticate } from "../features/auth/authSlice";
+
+import { Colors } from "../utils/colors";
+import { Alert } from "react-native";
+
 export default function Login() {
-  return (
-    <AuthContent
-      onAuthenticate={({ email, password }) => console.log({ email, password })}
-      isLogin
-    />
+  const { isAuthenticated, token } = useSelector(
+    (state: RootState) => state.auth
   );
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const logInHandler = async (credential: Credential) => {
+    try {
+      setIsAuthenticating(true);
+      await dispatch(
+        authenticate({
+          authMode: "signInWithPassword",
+          credential,
+        })
+      );
+    } catch (error) {
+      console.error("Fail to Log In", error);
+      Alert.alert(
+        "Authentication failed!",
+        "Could not log you in. Please check your credentials or try again later!"
+      );
+    } finally {
+      setIsAuthenticating(false);
+    }
+  };
+
+  if (isAuthenticating) {
+    return <ActivityIndicator animating={true} color={Colors.mainColor} />;
+  }
+
+  return <AuthContent onAuthenticate={logInHandler} isLogin />;
 }
--- screens/Signup.tsx
+++ screens/Signup.tsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
+import { ActivityIndicator } from "react-native-paper";
+
 import AuthContent from "../components/Auth/AuthContent";
 
+import { Credential } from "../types/Auth";
+
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store";
+import { authenticate } from "../features/auth/authSlice";
+
+import { Colors } from "../utils/colors";
+import { Alert } from "react-native";
+
 export default function SignUp() {
-  return (
-    <AuthContent
-      onAuthenticate={({ email, password }) => console.log({ email, password })}
-      isLogin={false}
-    />
-  );
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const signUpHandler = async (credential: Credential) => {
+    try {
+      setIsAuthenticating(true);
+      await dispatch(
+        authenticate({
+          authMode: "signUp",
+          credential,
+        })
+      );
+    } catch (error) {
+      console.error("Fail to Sign Up", error);
+      Alert.alert(
+        "Authentication failed!",
+        "Could not create user. Please check your credentials or try again later!"
+      );
+    } finally {
+      setIsAuthenticating(false);
+    }
+  };
+
+  if (isAuthenticating) {
+    return <ActivityIndicator animating={true} color={Colors.mainColor} />;
+  }
+
+  return <AuthContent onAuthenticate={signUpHandler} isLogin={false} />;
 }
